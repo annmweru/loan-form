ffc967094d2d44321007255bf109c763c34d9785
oyamoh-brian <oyamobrian8@gmail.com>
2021-09-10 15:30:38 +0300

    Employment Form

---
+++ src/app/person-form/person-form.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+import {FormBuilder} from "@angular/forms";
+
+@Component({
+  selector: "app-application"
+})
+export class ApplicationFormComponent {
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+}
